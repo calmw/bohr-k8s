b803db884f163cda48c70c0f0e9b4c45344582d1
calm <calm@calm.com>
2026-02-03 00:17:05 +0800

    .

--- explorer/clear.sh
+++ explorer/clear.sh
@@ -6,6 +6,9 @@ sleep 10
 kubectl delete deployment scan-stats -n bohr-prod
 echo "等待 10 秒..."
 sleep 10
+kubectl delete deployment scan-frontend -n bohr-prod
+echo "等待 10 秒..."
+sleep 10
 kubectl delete pvc scan-backend-dets-pvc -n bohr-prod
 echo "等待 10 秒..."
 sleep 10
