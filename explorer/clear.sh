f86de3aa5bbbedfa545ccccda20dfa2006fecaa5
calm <calm@calm.com>
2026-02-06 16:35:18 +0800

    .

--- explorer/clear.sh
+++ explorer/clear.sh
@@ -16,7 +16,3 @@ kubectl delete pvc scan-backend-dets-pvc -n bohr-prod
 echo "等待 10 秒..."
 sleep 10
 kubectl delete pvc scan-backend-logs-pvc -n bohr-prod
-echo "等待 10 秒..."
-sleep 10
-kubectl apply -f scan-backend-dets-pvc.yaml -n bohr-prod &&
-kubectl apply -f scan-backend-logs-pvc.yaml -n bohr-prod
