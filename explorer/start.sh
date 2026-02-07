aa20aae6281cf17bbf172713f54d3032be501198
calm <calm@calm.com>
2026-02-07 11:32:00 +0800

    .

--- explorer/start.sh
+++ explorer/start.sh
@@ -11,7 +11,9 @@ sleep 10
 kubectl apply -f scan-stats-deployment.yaml -n bohr-prod
 echo "等待 10 秒..."
 sleep 10
-kubectl apply -f scan-frontend-deployment.yaml -n bohr-prod &&
+kubectl apply -f scan-frontend-deployment.yaml -n bohr-prod
+echo "等待 10 秒..."
+sleep 10
 kubectl apply -f scan-visualizer-deployment.yaml -n bohr-prod &&
 kubectl apply -f scan-sig-provider-deployment.yaml -n bohr-prod &&
 kubectl apply -f scan-backend-service.yaml -n bohr-prod &&
